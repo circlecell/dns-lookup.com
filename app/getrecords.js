78a0e857e3bd102caaf4b79cb7b55728d2a2392a
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2017-06-01 14:50:10 +0300

    fix: Allow to use unicode domains

--- app/getrecords.js
+++ app/getrecords.js
@@ -1,9 +1,14 @@
 const dns = require('native-dns');
+const punycode = require('punycode');
 
 function getRecords(name, type = 'ANY') {
+
     return new Promise((resolve, reject) => {
         const req = new dns.Request({
-            question: new dns.Question({ name, type }),
+            question: new dns.Question({
+                name: punycode.toASCII(name),
+                type
+            }),
             server: { address: '8.8.8.8', port: 53, type: 'udp' },
             timeout: 5000
         });
